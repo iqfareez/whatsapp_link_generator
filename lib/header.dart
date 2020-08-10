a8e362bdc38d1ab8a0b5a91d559ed33f00c8dc39
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-08-10 23:29:22 +0800

    Initial commit

---
+++ lib/header.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WhatsApp link generator',
+      theme: ThemeData(
+
+        primarySwatch: Colors.teal.shade800,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
+
