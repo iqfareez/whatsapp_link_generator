017592c413f6e08f9383535ade517d872773c876
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-09-02 00:36:45 +0800

    Encoded url

--- lib/result.dart
+++ lib/result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ResultPage extends StatelessWidget {
   ResultPage({this.phoneNumber, this.message});
@@ -25,11 +26,60 @@ class ResultBody extends StatefulWidget {
 }
 
 class _ResultBodyState extends State<ResultBody> {
+  bool isSelectedPhone = true;
+  bool isSelectedMessage = true;
   @override
   Widget build(BuildContext context) {
+    final chips = [
+      FilterChip(
+        label: Text('Phone number'),
+        // avatar: FaIcon(FontAwesomeIcons.phone),
+        selected: isSelectedPhone,
+        onSelected: (bool value) {
+          setState(() {
+            isSelectedPhone = !isSelectedPhone;
+          });
+        },
+      ),
+      FilterChip(
+        label: Text('Message'),
+        // avatar: FaIcon(FontAwesomeIcons.envelopeOpenText),
+        selected: isSelectedMessage,
+        onSelected: (bool value) {
+          setState(() {
+            isSelectedMessage = !isSelectedMessage;
+          });
+        },
+      )
+    ];
     return Container(
-      child: Text(
-          'Phone Number is ${widget.phoneNum} and message is ${widget.message}'),
+      margin: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Wrap(
+              children: [
+                for (final chip in chips)
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: chip,
+                  )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Text(
+                'https://wa.me/${widget.phoneNum}/${Uri.encodeComponent(widget.message)}'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text('Action area'),
+          )
+        ],
+      ),
     );
   }
 }
