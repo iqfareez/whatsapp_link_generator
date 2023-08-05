237797ba7ea85d9d8387cf0245a31902ec922cfa
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-06 02:56:33 +0800

    :hammer: Regenerate latest Android folder template

--- android/app/src/main/java/live/iqfareez/whatsapp_link_generator/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package live.iqfareez.whatsapp_link_generator;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/live/iqfareez/whatsapp_link_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package live.iqfareez.whatsapp_link_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
